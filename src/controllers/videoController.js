da6620d78efbe6d6fc91bc89ce20ad389de3a523
sexychoco <gaeun819@naver.com>
2022-01-13 22:41:36 +0900

    add parameter

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,17 @@
 export const trending = (req,res) => res.send("Homepage Videos");
-export const edit = (req,res) => res.send("Edit");
-export const watch = (req,res) => res.send("Watch");
 
+export const edit = (req,res) => {
+    return res.send("Edit");
+}
+    
+export const see = (req,res) => {
+    return res.send(`Watch Video #${req.params.id}`);
+}
+
+export const search = (req,res) => res.send("search");
+
+export const upload = (req,res) => res.send("Upload");
+
+export const deleteVideo = (req,res) => {
+    return res.send("Delete Video");
+}
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { trending } from "../controllers/videoController";
-import { join } from "../controllers/userController";
+import { trending,search } from "../controllers/videoController";
+import { join,login } from "../controllers/userController";
+
 
 const globalRouter = express.Router();
 
 globalRouter.get("/",trending);
 globalRouter.get("/join",join);
+globalRouter.get("/search",search);
+globalRouter.get("/login",login);
 
 export default globalRouter;
