fc4a8e5b4b04b9e1a787d9ecadfc56589805c227
sexychoco <gaeun819@naver.com>
2022-01-13 15:33:27 +0900

    basic

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT=4000;
 
 const app=express();
+const logger = morgan("dev");
 
-const handleHome = (req,res) => {
-    return res.send("i still love you");
+const home = (req,res) => {
+    return res.send("hello");
 }
 
-const handleLogin = (req,res) => {
-    return res.send("You Login here.");
-}
-
-app.get("/",handleHome);
-app.get("/login",handleLogin);
+app.use(logger);
+app.get("/",home);
 
 
 const handleListening = () => console.log(`Server listening on port https://localhost:${PORT}🚀`);
