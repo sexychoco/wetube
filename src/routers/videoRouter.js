0206a3f482fa25117624b33766bc4a0aa3aa9fd5
sexychoco <gaeun819@naver.com>
2022-01-15 16:15:05 +0900

    add upload part

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,17 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+///컨트롤러를 만든 뒤 라우터를 만들어준다. 그리고 거기에 컨트롤러 사용///
+///그 다음 view 파일에 upload 템플릿을 base를 기반으로 생성///
 export default videoRouter;
