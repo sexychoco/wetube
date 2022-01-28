c72ba2a7d0aef74f129b124829fafb7158acb244
sexychoco <gaeun819@naver.com>
2022-01-28 20:41:01 +0900

    delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,7 +13,7 @@ const videoRouter = express.Router();
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 ///컨트롤러를 만든 뒤 라우터를 만들어준다. 그리고 거기에 컨트롤러 사용///
 ///그 다음 view 파일에 upload 템플릿을 base를 기반으로 생성///
 export default videoRouter;
