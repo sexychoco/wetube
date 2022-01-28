4ba97c5a09eff61b480e9e977f48f6718989f4c0
sexychoco <gaeun819@naver.com>
2022-01-28 22:35:14 +0900

    add search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
@@ -65,4 +65,18 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
 
   return res.redirect("/");
+};
+
+///req.query is for the URL data.///
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
