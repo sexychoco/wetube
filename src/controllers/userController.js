1eb7b07e4f2bdd11ec3ab92ca4b6449854e77949
sexychoco <gaeun819@naver.com>
2022-02-01 17:40:53 +0900

    upload profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -149,6 +149,7 @@ export const postEdit = async (req, res) => {
       user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
+    file,
   } = req;
   const updatedUser = await User.findByIdAndUpdate(
     _id,
