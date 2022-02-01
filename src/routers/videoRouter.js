395ad1990f1cad87e05f4403e612b0518b4686ee
sexychoco <gaeun819@naver.com>
2022-02-01 15:48:33 +0900

    Change password

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,25 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
 ///컨트롤러를 만든 뒤 라우터를 만들어준다. 그리고 거기에 컨트롤러 사용///
 ///그 다음 view 파일에 upload 템플릿을 base를 기반으로 생성///
 export default videoRouter;
