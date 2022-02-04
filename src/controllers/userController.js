c128ea6cffa65e5d9b403d70e199fb2f5abbbea2
sexychoco <gaeun819@naver.com>
2022-02-04 21:10:34 +0900

    css finish

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -210,7 +210,7 @@ export const see = async (req, res) => {
     },
   });
   if (!user) {
-    return res.status(404).render("404", { pageTitle: "User not found" });
+    return res.status(404).render("404", { pageTitle: "User not found." });
   }
   return res.render("users/profile", {
     pageTitle: user.name,
