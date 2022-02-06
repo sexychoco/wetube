12a6192e50dedf5d898043054eb7824253ec07f4
sexychoco <gaeun819@naver.com>
2022-02-06 23:01:36 +0900

    sending messages

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,6 @@
 import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
-  console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
@@ -10,8 +9,9 @@ export const localsMiddleware = (req, res, next) => {
 
 export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
-    next();
+    return next();
   } else {
+    req.flash("error", "Log in first.");
     return res.redirect("/login");
   }
 };
@@ -20,6 +20,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
   } else {
+    req.flash("error", "Not authorized");
     return res.redirect("/");
   }
 };
